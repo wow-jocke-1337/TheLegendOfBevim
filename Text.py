11102e8081ed6aa56e908ed5dbb45e7aeb2e51aa
William <william.ingo@skola.taby.se>
2022-12-13 18:28:55 +0100

    Nya grejer

---
+++ Text.py
@@ -0,0 +1,72 @@
+from time import sleep
+import random as rand
+import time
+import sys,time,random
+
+typing_speed = 50 #wpm
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+    print('')
+
+monsters=["Golem","Wolf","Bats","Undead","Lava eel"]
+locations=["Heart of The Mountain","Random Crack in The Wall","Nesting Site","Lava Falls","Storage Room"]
+
+class Player():
+    
+    def __init__(self, klass, lifes, capacity, armor):
+        self.lifes = lifes
+        self.scores = 0
+        self.armor = armor
+        self.did_hit = False
+        self.is_hitted = False
+        self.capacity = capacity
+        self.klass = klass
+
+    def print_info(self):
+        slow_type(f"""
+                Character: {self.klass}
+                Health: {self.lifes}/{self.lifes} Armour: {self.armor}/{self.armor} 
+                Capacity: {self.capacity}/{self.capacity}""") 
+
+    def fire(self):
+        pass
+       # Här sker "eldväxlingen"
+
+    def inc_scores(self): 
+        pass
+       # Här ska poängen öka
+
+    def get_shot(self):
+        pass
+        # Här ska antalet liv minska
+def begin():
+    print(f"""
+     _________  ___  ___  _______           ________  _______   ___      ___ _______   ________   ________  _______           ________  ___________ 
+    |\___   ___\|  \|\  \|\  ___ \         |\   __  \|\  ___ \ |\  \    /  /|\  ___ \ |\   ___  \|\   ____\|\  ___ \         |\   __  \|\  _______/
+    \|___ \  \_\ \  \|\  \ \   __/|        \ \  \|\  \ \   __/|\ \  \  /  / | \   __/|\ \  | \  \ \  \___|\ \   __/|         \ \  \ \  \ \  \____/ 
+         \ \  \ \ \   __  \ \  \_|/__       \ \   _  _\ \  \_|/_\ \  \/  / / \ \  \_|/_\ \  | \  \ \  \  __\ \  \_|/__        \ \  \ \  \ \   ___/
+          \ \  \ \ \  \ \  \ \  \_|\ \       \ \  |\   \ \  \_|\ \ \    / /   \ \  \_|\ \ \  |\ \  \ \  \|\ \ \  \_|\ \        \ \  \_\  \ \  \_/
+           \ \__\ \ \__\ \__\ \_______\       \ \__|\ _ \ \_______\ \__/ /     \ \_______\ \__|\ \__\ \______\ \_______\        \ \_______\ \__|
+            \|__|  \|__|\|__|\|_______|        \|__|\|__|\|_______|\|__|/       \|_______|\|__| \|__|\|______|\|_______|         \|_______|\|__| 
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             
+     ________  _______   ___      ___ ___  _____ ______   ________           _____ ______   ________  ________  ________  ________               
+    |\   __  \|\  ___ \ |\  \    /  /|\  \|\   _ \  _   \|\   ____\         |\   _ \  _   \|\   __  \|\   __  \|\   ____\|\   __  \              
+    \ \  \|\ /\ \   __/|\ \  \  /  / | \  \ \  \ \__\ \  \ \  \___|_        \ \  \ \__\ \  \ \  \ \  \ \  \ \  \ \  \___|\ \  \ \  \             
+     \ \   __  \ \  \_|/_\ \  \/  / / \ \  \ \  \_|__| \  \ \_____  \        \ \  \_|__| \  \ \  \ \  \ \   _  _\ \_____  \ \   __  \            
+      \ \  \|\  \ \  \_|\ \ \    / /   \ \  \ \  \    \ \  \|____|\  \        \ \  \    \ \  \ \  \ \  \ \  |\  \|_____|\  \ \  \ \  \           
+       \ \_______\ \_______\ \__/ /     \ \__\ \__\    \ \__\____\_\  \        \ \__\    \ \__\ \_______\ \__|\ _\ ____\_\  \ \__\ \__\          
+        \|_______|\|_______|\|__|/       \|__|\|__|     \|__|\_________\        \|__|     \|__|\|_______|\|__|\|__|\_________\|__|\|__|          
+                                                            \|_________|                                          \|_________|                   
+                                                                                                                                             """) 
+    
+    slow_type("""\nVälkommen till vårt spel! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""")
+begin()
+player_one = Player("Bevim", 100, 25, 0)
+player_one.print_info()
