dadadf1d9c7e48f0713e8a1d5dce88b8e977ce4d
Alfred <Alfred.hogerberg@skola.taby.se>
2022-12-13 11:22:59 +0100

    jesus

--- Classes.py
+++ Classes.py
@@ -1,5 +1,5 @@
 class Player():
-    def __init__(self, lifes):
+    def info(self, lifes):
         self.lifes = lifes
         self.scores = 0
         self.did_hit = False
@@ -9,13 +9,23 @@ class Player():
         pass
        # Här sker "eldväxlingen"
 
+    def Block(self):
+        pass
+       # Spelaren väljer att blocka nästa tur och försöka få monstret att bli stunned
+
     def lvl_up(self): 
         pass
        # Här ska lvl öka
 
-    def get_shot(self):
+    def get_attacked(self):
         pass
         # Här ska antalet liv minska
+
+    def Drink_potion(self):
+        pass
+
+    def Eat_food(self):
+        pass
 
 class Knight():
     def stats(HP, STR, ):
