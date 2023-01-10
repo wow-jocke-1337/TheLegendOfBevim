a885f49786ebce201ef3daf27844fdfbe49a42ac
Wiii-U <william.ingo@skola.taby.se>
2023-01-10 07:39:35 +0100

    Nya ändringar

--- TheLegendOfBevim.py
+++ TheLegendOfBevim.py
@@ -18,19 +18,22 @@ while True:
     animate_typing(Menu)
     x = int(input(""))
     if x == 1:
+        animate_typing()
+    elif x == 2:
         Player.print_info()
         time.sleep(2)
-    elif x == 2:
-        break
     elif x == 3:
+        animate_typing(background_info)
+        time.sleep(2)
+    elif x == 4:
         animate_typing("""
     Thank you for playing :) 
-    
+
     """)
         exit()
-
-
-Player.gets_attacked()
-if Player.HP < 0:
-    Player.die
+    else:
+        animate_typing("""
+    You faking stopid choose an actual alternative, not dreaming.
+    You should get a mirror, so you can see how dumb you are.
     
+    """)
