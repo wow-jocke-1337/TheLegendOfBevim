d6e241952d8da0627e60fb97567d96581961dc3f
Alfred <Alfred.hogerberg@skola.taby.se>
2023-01-11 12:05:34 +0100

    yufitif

--- Rum.py
+++ Rum.py
@@ -1,31 +1,53 @@
 from random import randint
 from Text import*
-
-def gen_room():
-    pass
-
 import random as rand
 
-boss_room = ["Morsans Lair"]
-chest_rooms = ["Windstrucken Barn ", "A Nesting site ", "Valley ", "Volcanic Caverns ", "Skyisland ", "Duskwoods ", "Graveyard ", "Niagara Falls ", "Ivory Tower ", "Bing bing bang "]
-starting_room = []
-monster_room = []
-trap_room = []
-chosen_room = []
-#Start rummet är alltid lika med 0. Start rummet ska poppas ur listan i början.
-#Resten av rummen ska få slumpade värden varje gång. 
+room_type = ["M","E","C","B","T"]
+final_boss_room = ["Morsans lair"]
+M_rooms = ["Bats","Wolf","Golem","Mountain lion","Slime"]
+E_rooms = ["Forest","Swamp","Lone Village","Cemetery"]
+C_rooms = ["Treasure room"]
+B_rooms = ["Archdemon","Bevins bror","What da hell!",""]
+T_rooms = ["Pitfall","Wall of spikes","Falling guillotine"]
 
-if "Windstrucken Barn " in chest_rooms:
-    starting_room.extend(chest_rooms[0])
-    chest_rooms.pop(0)
-    animate_typing("""\n
-    You arrive at a Windstrucken Barn, looking around you find that the barn is abandoned and the cause of your arrival seems at first a mystery.
-    Collecting yourself, you search the barn discovering nothing but that the light is out of reach, wherever you are this place does not come of as
-    merciful, but cursed and ancient in some way. 
-        Survive and successfully escape, but first you must choose one of the three doors currently infront of you.""")
-    time.sleep(1)
-    animate_typing("""\n
-    1: Right?
-    2: Middle?
-    3: Left? 
-        """)
+def gen_room():
+    room = random.choice(room_type)
+    if room == "M":
+        animate_typing('\nMonster room.')
+        monster = random.choice(M_rooms)
+        if monster == "Bats":
+            animate_typing('\nYou encountered a pack of bats!')
+        elif monster == "Wolf":
+            animate_typing('\nYou encountered a Wolf!')
+        elif monster == "Golem":
+            animate_typing('\nYou encountered a Golem!')
+        elif monster == "Mountain lion":
+            animate_typing('\nYou encountered a Mountain lion!')
+        elif monster == "Slime":
+            animate_typing('\nYou encountered a slime!')
+    elif room == "E":
+        animate_typing('\nThis room is empty.')
+    elif room == "C":
+        animate_typing('\nYou found a Treasure!')
+    elif room == "B":
+        animate_typing('\nBoss room.')
+        boss = random.choice(B_rooms)
+        if boss == "Archdemon":
+            animate_typing('\nYou encountered a Archdemon!')
+        elif boss == "Bevins bror":
+            animate_typing('\nYou encountered your brother, Bing CHilling!')
+        else:
+            animate_typing('\nWhat da hell!')
+    elif room == "T":
+        animate_typing('\nTrap room.')
+        trap = random.choice(T_rooms)
+        if trap == "Pitfall":
+            animate_typing('\nYou fell!')
+        elif trap == "Wall of spikes":
+            animate_typing('\nYou got spiked!')
+        else:
+            animate_typing('\nSevered you are!')
+gen_room()
+
+def room_count():
+    pass
