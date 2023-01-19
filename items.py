47d3e3588971ac93f42e689681e40a22232b6013
Krillecummings <kristoffer.cummings@skola.taby.se>
2023-01-19 10:17:31 +0100

    hej

--- items.py
+++ items.py
@@ -3,3 +3,9 @@ from Classes import*
 
 class IronSword():
     Dmg = 3 + Player.STR * 0.25
+
+class Bow():
+    Dmg= 3 + Player.STR * 0.15 + Player.DEX * 0.70 + Player.INT * 0.1
+
+class Magical_Staff():
+    Dmg= 3 + Player.STR * 0.10 + Player.DEX * 0.30 + Player.INT *0.6
